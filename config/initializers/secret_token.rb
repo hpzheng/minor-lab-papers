7aebe52ba683e4bd1fcd78afccd18f3186c21e18
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-03-22 14:41:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Milapa::Application.config.secret_token = '246748235ef358c64e769ef0ae0f5bc5036353124816b1ecd4c7701f0c2d11642eca7b9e18df3181989a3367e27d6c0e4667cd449a4653e2d12facbd9e5b4dc2'
