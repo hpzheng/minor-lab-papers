51d7a6cae6d615141b4f13baf9c31bb9a5d52a39
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-04-02 12:11:36 -0400

    updated the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -59,7 +59,7 @@ Milapa::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'publications#index'
 
   # See how all your routes lay out with "rake routes"
 
