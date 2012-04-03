e048204d9aba8926eb14af897aaed79db0089f09
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-04-03 15:58:52 -0400

    updated the homepage'

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120402154657) do
+ActiveRecord::Schema.define(:version => 20120403194336) do
 
   create_table "auth_group", :force => true do |t|
     t.string "name", :limit => 80, :null => false
@@ -242,6 +243,7 @@ ActiveRecord::Schema.define(:version => 20120402154657) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
