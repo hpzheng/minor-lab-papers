b861650c06fba8fbc58bf2be892d6b40c6445b36
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-04-24 13:25:06 -0400

    a few routing changes

--- app/models/publication_attachment.rb
+++ app/models/publication_attachment.rb
@@ -7,6 +7,8 @@ class PublicationAttachment < ActiveRecord::Base
   validates :publication_id, :presence => true
   validates :version, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
 
+  default_scope :order => 'updated_at DESC'
+
   def file_name
     return self.attachment.to_s.split('/').last
   end
