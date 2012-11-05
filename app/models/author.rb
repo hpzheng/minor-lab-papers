02f78e7be1fd7bde763e118c44628da38221e244
Matt D <matt_d@iwonka.med.virginia.edu>
2012-11-05 17:12:47 -0500

    unknown update to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -11,19 +11,19 @@ class Author < ActiveRecord::Base
 
   default_scope :order => 'firstname'
 
-  has_many :statuses, foreign_key: "person_responsible_id",
-                                     class_name: "PubStatusJcn"
-  has_many :contrib_papers, through: :statuses,
-                            #foreign_key: "publication_id",
-                            primary_key: "publication_id",
-                            class_name: "Publication",
-                            source: :publication
-  has_many :first_auth_papers, foreign_key: "first_author_id",
-                               class_name: "Publication"
-  has_many :second_auth_papers, foreign_key: "second_author_id",
-                               class_name: "Publication"
-  has_many :third_auth_papers, foreign_key: "third_author_id",
-                               class_name: "Publication"
+  has_many :statuses, :foreign_key => "person_responsible_id",
+                                     :class_name => "PubStatusJcn"
+  #has_many :contrib_papers, through: :statuses,
+  #                          #foreign_key: "publication_id",
+  #                          primary_key: "publication_id",
+  #                          class_name: "Publication",
+  #                          source: :publication
+  has_many :first_auth_papers, :foreign_key => "first_author_id",
+                               :class_name => "Publication"
+  has_many :second_auth_papers, :foreign_key => "second_author_id",
+                               :class_name => "Publication"
+  has_many :third_auth_papers, :foreign_key => "third_author_id",
+                               :class_name => "Publication"
 
   def auth_name
     if self.lastname.nil? || self.lastname.empty? || self.lastname == " "
--- lib/development_mail_interceptor.rb
+++
@@ -1,8 +0,0 @@
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.subject = "#{message.to} #{message.subject}"
-    #message.to = "matthewwdemas+RailsInterceptor@gmail.com"
-    message.to = "matt_d@iwonka.med.virginia.edu"
-  end
-end
-
