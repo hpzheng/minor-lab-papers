050d238411793157513b1724311a6af786b8402e
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-11-30 13:49:17 -0500

    default scope for manu upload

--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -2,8 +2,8 @@ class Manuscript < ActiveRecord::Base
 
   attr_accessible :publication_id, :title, :version, :attachment, :comment,:manuscript
 
+  default_scope :order => "updated_at DESC"
   default_scope :limit => "1"
-                  
 
   mount_uploader :manuscript, PublicationDocumentUploader
 
