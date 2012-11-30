ee4b3b837a5a1ed5ca1777ec8260275ebefbeaec
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-11-30 12:09:56 -0500

    updated manuscript model to mirror attachment model

--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -1,2 +1,14 @@
 class Manuscript < ActiveRecord::Base
+
+  attr_accessible :publication_id, :title, :version, :attachment, :comment
+
+  mount_uploader :manuscript, PublicationDocumentUploader
+
+  belongs_to :publication
+
+  validates :publication_id, :presence => true
+  validates :version, :presence => true, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  validates :manuscript, :presence => true
+
+
 end
