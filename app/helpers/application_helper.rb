3c05ba807fa205c8370f9bd4e6671b83776f2bfa
Heping Zheng <dust@iwonka.med.virginia.edu>
2012-12-12 09:03:46 -0500

    a number of stylistic changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,18 @@ module ApplicationHelper
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
+
+=begin
+  def construct_active_nav(opts={})
+    opts.each do |key, value|
+    if controller.controller_name == 'value[0]'
+      html << "<li class='active'><%= link_to #{value[1]}, #{kkkk
+  end
+
+# On condition mute text in show view
+  def
+    some code goes here
+    input name and attribute to test
+
+=end
 end
